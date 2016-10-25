06e41ac7548fb15cce8259e28507dd487e9138eb
killie <jon.ole@killie.org>
2016-10-25 19:01:10 +0200

    Loading users, years, months, days and files on startup

--- server/app.py
+++ server/app.py
@@ -1,4 +1,36 @@
 from flask import Flask, render_template
+import os
+
+def getRootFromConfig(file):
+    """Opening config file to find value of output setting."""
+    f = open(file)
+    for line in f:
+        if (line.startswith("output=")):
+            return line[7:].strip()
+    return ""
+
+def getFileList(root):
+    """Load all known files into array reverse sorted on date.
+    First level is user, second is year, month, day, photo."""
+    users = {"public": {}}
+    for user in os.walk(root).next()[1]:
+        users[user] = {}
+
+    for user in users.keys():
+        if (os.path.isdir(root + "/" + user)):
+            for year in os.walk(root + "/" + user).next()[1]:
+                users[user][year] = {}
+                for month in os.walk(root + "/" + user + "/" + year).next()[1]:
+                    users[user][year][month] = {}
+                    for day in os.walk(root + "/" + user + "/" + year + "/" + month).next()[1]:
+                        users[user][year][month][day] = []
+                        for path, dirs, files in os.walk(root + "/" + user + "/" + year + "/" + month + "/" + day):
+                            for filename in files:
+                                if (filename.endswith(".txt") == False):
+                                    users[user][year][month][day].append(filename)
+
+    return users
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +38,9 @@ def main():
     return render_template("index.html")
 
 if __name__ == "__main__":
+    files = getFileList(getRootFromConfig("zucasa.rc"))
+    print files
     app.run()
+
+
+
