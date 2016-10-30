3fc31b581a727757348b1d4e352bae19cc7c9a6d
Jon Ole Killie <jon.ole@killie.org>
2016-10-30 19:37:35 +0100

    Adding navigation to view page

--- server/static/zucasa.js
+++ server/static/zucasa.js
@@ -8,6 +8,13 @@ $(".sidebar li a").click(function (e) {
 $("img.thumbnail").click(function (e) {
     var src = e.target.src;
     var url = src.substring(0, src.length - 4);
-    url = url.replace("/static/import", "");
-    window.location.href = url;
+    window.location.href = url.replace("/static/import", "");
+});
+
+// Show original when clicking zoom on photo
+$("div.zoom").click(function (e) {
+    var src = $(e.target.parentElement).css("background-image");
+    src = src.replace('url("', '').replace('")', '');
+    window.location.href = src;
+
 });
