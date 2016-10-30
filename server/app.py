0cc81e2f63ea5514478463f317d01511d9e226fa
Jon Ole Killie <jon.ole@killie.org>
2016-10-30 13:28:48 +0100

    Including surrounding thumbnails in view mode.

--- server/app.py
+++ server/app.py
@@ -4,13 +4,14 @@ from os import path, getcwd
 import calendar
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-from core import get_file_list, Photo
+from core import get_files_as_map, sort_photos_into_array, Photo
 
 # Globals
 
 app = Flask(__name__)
 root = getcwd() + "/server/static/import"
-files = get_file_list(root)
+files = get_files_as_map(root)
+photos = sort_photos_into_array(files)
 
 # Routes
 
@@ -24,13 +25,28 @@ def user(user):
 
 @app.route("/<user>/<year>/<month>/<day>/<num>")
 def view(user, year, month, day, num):
+    # Create instance of Photo class and load original from disk into cache
     photo = Photo(user, year, month, day, num)
     photo.load_photo()
-    thumbnails = [ \
-        "/static/import/jonole/2006/01/21/1.jpg", \
-        "/static/import/jonole/2006/01/21/2.jpg", \
-        "/static/import/jonole/2006/01/21/3.jpg", \
-        "/static/import/jonole/2006/01/21/4.jpg"]
+
+    # Identify photo in sorted array and add surrounding photos to thumbnails array
+    thumbnails = []
+    i = 0
+    for p in photos[user]:
+        print "Index " + str(i) + " len: " + str(len(photos[user]))
+        if (p.date_taken == photo.date_taken):
+            if (p.num == int(num)):
+                if (i > 2):
+                    thumbnails.append(photos[user][i - 2].get_thumbnail_src())
+                if (i > 1):
+                    thumbnails.append(photos[user][i - 1].get_thumbnail_src())
+                thumbnails.append(photos[user][i].get_thumbnail_src())
+                if (i + 1 < len(photos[user])):
+                    thumbnails.append(photos[user][i + 1].get_thumbnail_src())
+                if (i + 2 < len(photos[user])):
+                    thumbnails.append(photos[user][i + 2].get_thumbnail_src())
+        i += 1
+
     return render_template("view.html", photo=photo.original_src, thumbnails=thumbnails)
 
 # Template helpers
--- server/core.py
+++ server/core.py
@@ -1,39 +1,71 @@
 import os
 from shutil import copyfile
+import datetime
+import operator
 
-def get_file_list(root):
-    """Load all known files into array reverse sorted on date.
-    First level is user, second is year, month, day, photo."""
+def get_files_as_map(root):
+    """Load all known image files into map with user, year, month and day as keys,
+    then an array of numbers for photos taken on that day."""
 
-    users = {"public": {}}
+    photos = {"public": {}}
     for user in os.walk(root).next()[1]:
-        users[user] = {}
+        photos[user] = {}
 
-    for user in users.keys():
+    for user in photos.keys():
         if (os.path.isdir(root + "/" + user)):
             for year in os.walk(root + "/" + user).next()[1]:
-                users[user][year] = {}
+                photos[user][year] = {}
                 for month in os.walk(root + "/" + user + "/" + year).next()[1]:
-                    users[user][year][month] = {}
+                    photos[user][year][month] = {}
                     for day in os.walk(root + "/" + user + "/" + year + "/" + month).next()[1]:
-                        users[user][year][month][day] = []
+                        photos[user][year][month][day] = []
                         for path, dirs, files in os.walk(root + "/" + user + "/" + year + "/" + month + "/" + day):
                             for filename in files:
                                 if (filename.endswith(".txt") == False):
-                                    users[user][year][month][day].append(filename)
+                                    photos[user][year][month][day].append(filename)
+                        
+                        # Sort file numbers array
+                        photos[user][year][month][day].sort()
+                        print photos[user][year][month][day]
 
-    return users
+    return photos
+
+def sort_photos_into_array(m):
+    """Takes map of photos and creates instances of Photo class which are added to user map,
+    where each user has an array of photos sorted by descending date."""
+
+    photos = {"public": []}
+    for user in m.keys():
+        photos[user] = []
+        for year in m[user].keys():
+            for month in m[user][year].keys():
+                for day in m[user][year][month].keys():
+                    for f in m[user][year][month][day]:
+                        num = f[:f.index(".")]
+                        photo = Photo(user, year, month, day, num)
+                        photo.ext = f[f.index(".") + 1:]
+                        photos[user].append(photo)
+
+        photos[user].sort(key=operator.attrgetter("num"))
+        photos[user].sort(key=operator.attrgetter("date_taken"), reverse=True)
+
+    return photos
 
 class Photo:
     """Holds reference to a photo via user, year, month, day and number. 
     Has methods to get metainfo and original image."""
+
+    ext="" # File extension
 
     def __init__(self, user, year, month, day, num):
         self.user = user
         self.year = year
         self.month = month
         self.day = day
-        self.num = num
+        self.num = int(num)
+
+        # Create datetime object to sort photos
+        self.date_taken = datetime.date(int(year), int(month), int(day))
 
         # Where to find meta info
         self.meta_info_file = os.getcwd() + "/server/static/import/" + \
@@ -66,8 +98,10 @@ class Photo:
         copyfile(directory + "/" + file_name, self.cache_file)
         return self.cache_file[self.cache_file.index("/static/cache/"):]
 
-
-
+    def get_thumbnail_src(self):
+        """Return reference to thumbnail file for this photo."""
+        return "/static/import/" + self.user + "/" + self.year + "/" + self.month + "/" + self.day + \
+            "/" + str(self.num) + "." + self.ext
 
 
 
