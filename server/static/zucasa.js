b4ebf33d81eca6b52cb5a067c9caefe054bc10c3
killie <jon.ole@killie.org>
2016-11-23 20:10:54 +0100

    Adding click handlers on photo prev/next buttons

--- server/static/zucasa.js
+++ server/static/zucasa.js
@@ -6,16 +6,22 @@ $(".sidebar li a").click(function (e) {
 
 // Open photo when clicking thumbnail
 $("img.thumbnail").click(function (e) {
-    var src = e.target.src;
-    var url = src.substring(0, src.length - 4);
-    window.location.href = url.replace("/static/import/photos", "");
+    openThumbnail(e.target.src);
 });
 
 // Show original when clicking zoom on photo
-$("div.zoom").click(function (e) {
+$(".photo .zoom").click(function (e) {
     var src = $(e.target.parentElement).css("background-image");
     src = src.replace('url("', '').replace('")', '');
     window.location.href = src;
+});
+
+$(".photo .prev").click(function (e) {
+    openThumbnail(getThumbnailUrl(2));
+});
+
+$(".photo .next").click(function (e) {
+    openThumbnail(getThumbnailUrl(4));
 });
 
 // Clicking close on view page goes back to /<user>
@@ -23,6 +29,16 @@ $("#view .close").click(function (e) {
     var url = window.location.href;
     window.location.href = url.substring(0, url.length - 16);
 });
+
+function getThumbnailUrl(index) {
+    return $("img.thumbnail").eq(index).attr("src");
+}
+
+function openThumbnail(url) {
+    // Remove extension, but it can be more than 3 chars
+    var href = url.substring(0, url.length - 4);
+    window.location.href = href.replace("/static/import/photos", "");
+}
 
 // Clicking cancel on config page goes to previous page
 $("#config .cancel").click(function (e) {
