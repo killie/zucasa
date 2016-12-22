141d5d195a26afb357fd4cabf7bc6e75f6b37c24
Jon Ole Killie <jon.ole@killie.org>
2016-12-22 11:50:29 +0100

    Moving comments

--- zucasa.sh
+++ zucasa.sh
@@ -2,13 +2,12 @@
 
 # Get local IP address
 LOCAL_IP=$(grep -Po 'src \K.*(?= cache)' <<< $(ip route get 1))
-
-# if RTNETLINK answers: Network is unreachable then use blank (localhost)
 SIZE=${#LOCAL_IP}
 
-# Launch web server
 if [ $SIZE = 0 ]; then
+    # if RTNETLINK answers: Network is unreachable then use blank (localhost)
     python server/app.py ""
 else
+    # Launch web server
     python server/app.py $LOCAL_IP
 fi
