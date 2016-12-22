d80314196021a12a2165fccb48c0dbedc025d2b1
Jon Ole Killie <jon.ole@killie.org>
2016-12-22 11:48:40 +0100

    Using blank address when there is no ip

--- zucasa.sh
+++ zucasa.sh
@@ -4,6 +4,11 @@
 LOCAL_IP=$(grep -Po 'src \K.*(?= cache)' <<< $(ip route get 1))
 
 # if RTNETLINK answers: Network is unreachable then use blank (localhost)
+SIZE=${#LOCAL_IP}
 
 # Launch web server
-python server/app.py $LOCAL_IP
+if [ $SIZE = 0 ]; then
+    python server/app.py ""
+else
+    python server/app.py $LOCAL_IP
+fi
